11f2b70f800afd4959324a1dbe036c5992c1a0a4
Kevin Sijbers <info@aurop.net>
2020-05-14 09:07:10 +0200

    AWS Amplify

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Amplify from '@aws-amplify/core';
+
+/** Presentational */
+import App from './Components/App';
+import { GlobalStyles } from './global';
+
+/** Amplify config */
+import awsconfig from './aws-exports';
+
+/** Service worker */
 import * as serviceWorker from './serviceWorker';
 
+/** Configure amplify */
+Amplify.configure(awsconfig);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <>
+    <GlobalStyles />
     <App />
-  </React.StrictMode>,
+  </>,
   document.getElementById('root')
 );
 
@@ -15,3 +27,5 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+//based on:
+//https://blog.logrocket.com/authentication-react-apps-aws-amplify-cognito/
